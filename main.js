ea4b88a78bf5f0cd287751a063736e66d5cac744
langyo <langyo.china@gmail.com>
2020-04-24 18:14:55 +0800

    [Feature] More event emitters.

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
 import { fork } from 'child_process';
+import EventEmitter from 'events';
 
+let emitter = new EventEmitter();
 let child = fork('./child.js');
 
-child.on('message', msg => {
-  console.log('[Parent]', msg);
-  child.send(msg + 1);
-});
+child.on('message', n => emitter.emit('message', n));
+
+let listener1 = n => {
+  console.log('Parent 1', n);
+  emitter.off('message', listener1);
+  child.send(2);
+};
+
+emitter.on('message', listener1);
+
+let listener2 = n => {
+  console.log('Parent 2', n);
+  emitter.off('message', listener2);
+};
+emitter.on('message', listener2);
 
 child.send(1);
